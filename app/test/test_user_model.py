03633da5e121dac702d90912d03e1d6e1d3f618d
ollydbg <b.afellah.2018@gmail.com>
2024-08-21 04:34:16 +0000

    init

--- app/test/test_user_model.py
+++ app/test/test_user_model.py
@@ -2,16 +2,16 @@ import unittest
 
 from app.main.models.database import db
 from app.main.models.User import User
+from app.test.base import BaseTestCase
 
 
-class TestUserModel:
+class TestUserModel(BaseTestCase):
     def test_1(self):
         user = User(
             email="test@test.com",
             password="test",  # noqa: S106
         )
-        db.session.add(user)
-        db.session.commit()
+
         self.assertTrue(True)
 
 
